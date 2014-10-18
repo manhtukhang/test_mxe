584f8a396fa96bf6b98faacf11ddd45f4c732027
MT <manhtuvjp@gmail.com>
2014-10-18 07:07:43 +0000

    Travis build 60 pushed to bin

---
+++ usr/include/isl/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _ISL_INCLUDE_ISL_STDINT_H
+#define _ISL_INCLUDE_ISL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "isl 0.12.2"
+/* generated using gnu compiler gcc (Ubuntu 4.8.1-2ubuntu1~12.04) 4.8.1 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
---
+++ usr/lib/gcc/i686-w64-mingw32.static/4.9.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/travis/mxe/tmp-gcc-i686-w64-mingw32.static/gcc-4.9.1/configure --target=i686-w64-mingw32.static --build=x86_64-unknown-linux-gnu --prefix=/home/travis/mxe/usr --libdir=/home/travis/mxe/usr/lib --enable-languages=c,c++,objc,fortran --enable-version-specific-runtime-libs --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --disable-shared --disable-multilib --without-x --disable-win32-registry --enable-threads=win32 --disable-libgomp --with-cloog=/home/travis/mxe/usr --with-gmp=/home/travis/mxe/usr --with-isl=/home/travis/mxe/usr --with-mpc=/home/travis/mxe/usr --with-mpfr=/home/travis/mxe/usr --with-as=/home/travis/mxe/usr/bin/i686-w64-mingw32.static-as --with-ld=/home/travis/mxe/usr/bin/i686-w64-mingw32.static-ld --with-nm=/home/travis/mxe/usr/bin/i686-w64-mingw32.static-nm";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch_32", "pentiumpro" }, { "arch_64", "x86-64" } };
